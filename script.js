d37cd85c27a3a3553fff03f30f9f68aa942fab5b
Leon Forsythe <lvforsythe95@gmail.com>
2024-01-21 21:38:12 -0500

    started a script file with initial element selection and calculate function

---
+++ script.js
@@ -0,0 +1,16 @@
+const suggestedServing = document.getElementById("servingSizeG")
+const servingProtein = document.getElementById("servingP") 
+const servingCarb = document.getElementById("servingCarb") 
+const servingSatFat = document.getElementById("servingSatFat") 
+const userSizeG = document.getElementById("userSizeG")
+const submit = document.getElementById("submit")
+
+const calculateMacros = (e) => {
+    e.preventDefault()
+
+    const ratio 
+
+    const userCalories = suggestedServing.value 
+}
+
+submit.addEventListener("click", calculateMacros)
