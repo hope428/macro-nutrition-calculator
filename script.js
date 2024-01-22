49452097f1cb3659bb1657c7cefa2ea8a1c213b0
Leon Forsythe <lvforsythe95@gmail.com>
2024-01-21 21:48:22 -0500

    completed calculation function

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const suggestedServing = document.getElementById("servingSizeG")
-const servingProtein = document.getElementById("servingP") 
-const servingCarb = document.getElementById("servingCarb") 
-const servingSatFat = document.getElementById("servingSatFat") 
-const userSizeG = document.getElementById("userSizeG")
-const submit = document.getElementById("submit")
+const suggestedServing = document.getElementById("servingSizeG");
+const servingCal = document.getElementById("servingCal");
+const servingProtein = document.getElementById("servingP");
+const servingCarb = document.getElementById("servingCarb");
+const servingSatFat = document.getElementById("servingSatFat");
+const userSizeG = document.getElementById("userSizeG");
+const submit = document.getElementById("submit");
 
 const calculateMacros = (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const ratio 
+  const amount = userSizeG.value / suggestedServing.value;
 
-    const userCalories = suggestedServing.value 
-}
+  const userCalories = servingCal.value * amount;
+  const userProtein = servingProtein.value * amount;
+  const userCarb = servingCarb.value * amount;
+  const userSatFat = servingSatFat.value * amount;
 
-submit.addEventListener("click", calculateMacros)
+  console.log(
+    `Calories: ${userCalories}\nProtein: ${userProtein}\nCarbs: ${userCarb}\nSaturated Fat: ${userSatFat}`
+  );
+};
+
+submit.addEventListener("click", calculateMacros);
