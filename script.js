74ed3363748b4f58989bc5c4556b90fdc114ce16
Leon Forsythe <lvforsythe95@gmail.com>
2024-01-22 19:28:57 -0500

    reconfigured users nutrition info to be an object. created a new function to display the output to the html page

--- script.js
+++ script.js
@@ -5,20 +5,32 @@ const servingCarb = document.getElementById("servingCarb");
 const servingSatFat = document.getElementById("servingSatFat");
 const userSizeG = document.getElementById("userSizeG");
 const submit = document.getElementById("submit");
+let userNutrition = {calories: 0, protein: 0, fat: 0, carbs: 0}
 
 const calculateMacros = (e) => {
   e.preventDefault();
 
   const amount = userSizeG.value / suggestedServing.value;
 
-  const userCalories = servingCal.value * amount;
-  const userProtein = servingProtein.value * amount;
-  const userCarb = servingCarb.value * amount;
-  const userSatFat = servingSatFat.value * amount;
+  userNutrition.calories = servingCal.value * amount;
+  userNutrition.protein = servingProtein.value * amount;
+  userNutrition.carbs = servingCarb.value * amount;
+  userNutrition.fat = servingSatFat.value * amount;
 
-  console.log(
-    `Calories: ${userCalories}\nProtein: ${userProtein}\nCarbs: ${userCarb}\nSaturated Fat: ${userSatFat}`
-  );
+  displayMacros()
 };
+
+const displayMacros = (e) => {
+  const display = document.getElementById("output")
+  display.innerHTML = ""
+
+  for(const key in userNutrition){
+    console.log(`${key}: ${userNutrition[key]}`)
+
+    const btn = document.createElement('button')
+    btn.innerHTML = `${key}: ${userNutrition[key]}`
+    display.append(btn)
+  }
+}
 
 submit.addEventListener("click", calculateMacros);
